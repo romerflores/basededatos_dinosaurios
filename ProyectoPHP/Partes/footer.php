6bc76838cc94e7ccc0d8ec598df0654a1d1e960b
Romer <107067537+romerflores@users.noreply.github.com>
2024-04-26 02:37:17 -0400

    Delete ProyectoPHP directory

--- ProyectoPHP/Partes/footer.php
+++
@@ -1,9 +0,0 @@
-    <div class="pie_de_pagina" style="background: #191919; position: fixed; bottom: 0;width: 100%;height: 15%; justify-content: center; display: flex; padding-top: 1%;padding-bottom: 2%;" >
-        <div class="grupo">
-            <h1 style="font-family: 'Berlin Sans FB';color: aliceblue">Nombre: Romer Flores Condori CI:13085158</h1>
-
-        </div>
-    </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
